93383f3b6d337ab83d09e3c6c72c7feda6ba2718
mikolas <mikolas.janota@gmail.com>
2020-02-17 13:42:24 +0000

    minor improvements in testing scripts

--- labs/lab01/test.sh
+++ labs/lab01/test.sh
@@ -8,26 +8,31 @@
 if [[ $# != 1 ]] ; then
     echo "Usage: $0 <exercise-name>"
     echo "Example: ./${0} ex01"
-    exit 100
+    exit 1
 fi
 
 prog_name="${1}"
 cfile="${1}".c
 test_in="tests/${1}.in"
 test_out="tests/${1}.out"
-diff_opts="--ignore-space-change --ignore-blank-lines"
-#--color --ignore-trailing-space
+diff_opts=""
+
+# use color for diff, if supported
+if diff --help | grep -q -w -e '--color'
+then
+    diff_opts="${diff_opts} --color"
+fi
 
 if [ ! -f "${cfile}" ]; then
     echo "ERROR: file ${cfile} not found!"
-    exit 100
+    exit 1
 fi
 
 gcc -ansi -Wall -Wextra -pedantic -o ${prog_name} ${cfile}
 rv=$?
 if [ ${rv} != 0 ]; then
     echo "ERROR: Compilation failed!"
-    exit 100
+    exit 1
 else
     echo "Program successfully compiled..."
 fi
@@ -37,20 +42,24 @@ student_out=/tmp/in_${stamp}
 ./${prog_name} <${test_in} | sed -e 's/\r$//' > ${student_out}
 rv1=$?
 if [ ${rv} != 0 ]; then
-    echo "ERROR: Program did not return 0!"
-    exit 100
+    echo "ERROR: Compiled program did not return 0!"
+    exit 1
 else
-    echo "Program successfully ran..."
+    echo "Compiled program successfully ran..."
 fi
 
 if [ ! -f "${student_out}" ]; then
     echo "ERROR: The output of the exercise was not created (file ${student_out})!"
-    exit 100
-fi
-if diff ${diff_opts} ${student_out} ${test_out}; then
-    echo "Test PASSED!"
-else
-    echo "Test FAILURE!"
+    exit 1
 fi
 
+diff ${diff_opts} ${student_out} ${test_out}
+rv_diff=$?
 rm -f ${student_out}
+
+if [ ${rv_diff} == 0 ]; then
+    echo "Test ${test_in} PASSED!"
+else
+    echo "Test ${test_in} FAILURE!"
+    exit 1
+fi
--- labs/lab01/test_all.sh
+++ labs/lab01/test_all.sh
@@ -5,8 +5,16 @@
 # Created on:  Mon Feb 18 14:00:31 STD 2019
 # Copyright (C) 2019, Mikolas Janota
 #
+failures=0
 for e in ex01 ex02; do
     echo '====' "${e}"
     ./test.sh "${e}"
+    let failures="$?+${failures}"
     echo '===='
 done
+echo FAILURES: $failures
+if [ ${failures} == 0 ]; then
+    echo "**********************************"
+    echo "*All tests passed! Great success!*"
+    echo "**********************************"
+fi
