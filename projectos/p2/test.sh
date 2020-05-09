ace66b72368e27828cfd6489fb999ef94a330197
mikolas <mikolas.janota@gmail.com>
2020-05-09 17:44:37 +0100

    fixing portability issues with  test.sh

--- projectos/p2/test.sh
+++ projectos/p2/test.sh
@@ -5,6 +5,12 @@
 # Created on:  Sun Feb 17 13:31:59 STD 2019
 # Copyright (C) 2019, Mikolas Janota
 #
+
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 if [ $# -lt 2 ] ; then
     echo "Usage: $0 <test-dir> <src-file1> <src-file2> ..."
     echo "Example: ${0} testes-enunciado *.c"
@@ -35,7 +41,7 @@ fi
 ${CC} -o ${prog_name} $*
 rv_compile=$?
 if [ ${rv_compile} != 0 ]; then
-    echo "ERROR: Compilation failed!"
+    echo -e "${RED}ERROR: Compilation failed!${NC}"
     exit 1
 else
     echo "Program successfully compiled..."
@@ -46,16 +52,18 @@ for test_in in `ls -rS ${test_dir}/*.in`; do
     test_out="${test_in%.in}.out"
     stamp="${RANDOM}${RANDOM}"
     student_out=/tmp/in_${stamp}
-    ${TIME_COMMAND} ./${prog_name} <${test_in} >${student_out} 2>/dev/null
+    bash -c "${TIME_COMMAND} ./${prog_name} <${test_in} >${student_out}"
     rv_student=$?
 
     if [ ! -f "${student_out}" ]; then
-        echo "ERROR: The output of the exercise was not created (file ${student_out})!"
+        echo -e "${RED}ERROR: The output of the exercise was not created (file ${student_out})!${NC}"
+        rm -f ${prog_name}
         exit 1
     fi
 
     if [ ${rv_student} != 0 ]; then
-        echo "ERROR: Program did not return 0!"
+        echo -e "${RED}ERROR: Program did not return 0!\n${NC}"
+        rm -f ${prog_name}
         rm -f ${student_out}
         exit 1
     else
@@ -67,9 +75,10 @@ for test_in in `ls -rS ${test_dir}/*.in`; do
     rm -f ${student_out}
 
     if [ ${rv_diff} == 0 ]; then
-        echo "Test ${test_in} PASSED!"
+        echo -e "${GREEN}Test ${test_in} PASSED!\n${NC}"
     else
-        echo "Test ${test_in} FAILURE!"
+        echo -e "${RED}Test ${test_in} FAILURE!\n${NC}"
+        rm -f ${prog_name}
         exit 1
     fi
 done
